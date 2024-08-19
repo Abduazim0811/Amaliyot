8e9e96089b3aa0e76b1ac938dbba06b5f4a25f4d
Abduazim <yusupovabduazim0811@gmail.com>
2024-08-19 11:49:40 +0500

    first

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type PingResponse struct {
+	Message string `json:"message"`
+}
+
+func PingHandler(c *gin.Context){
+	for {
+		response := PingResponse{Message: "pong"}
+		c.JSON(http.StatusOK, response)
+	}
+}
+func main(){
+	r := gin.Default()
+
+	r.GET("/ping", PingHandler)
+
+	r.Run(":8080")
+}
